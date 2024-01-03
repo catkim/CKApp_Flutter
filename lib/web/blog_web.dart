b4522387df2746bc5e770627af0c8c97499aff30
catkim <abbytherabbit@gmail.com>
2024-01-02 20:21:39 -0800

    updating blog pages for web and mobile

---
+++ lib/web/blog_web.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class BlogWeb extends StatefulWidget {
+  const BlogWeb({super.key});
+
+  @override
+  State<BlogWeb> createState() => _BlogWebState();
+}
+
+class _BlogWebState extends State<BlogWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
